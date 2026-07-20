657905b50d8716b667a7d1c3ccfd910c71354fb6
alharamsweetandbakers-droid <alharamsweetandbakers@gmail.com>
2026-07-20 23:38:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+// Scroll To Top Button
+
+const scrollTop = document.getElementById("scrollTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        scrollTop.classList.add("show");
+
+    }else{
+
+        scrollTop.classList.remove("show");
+
+    }
+
+});
