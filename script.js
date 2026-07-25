dff584e999d022139e58e364b91946718e1ab3ec
alharamsweetandbakers-droid <alharamsweetandbakers@gmail.com>
2026-07-25 23:24:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+// Close menu after clicking any link
+
+const links = document.querySelectorAll("#nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
