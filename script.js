fb04367c8814e451c2e56b3e951260e4777d1a1f
alharamsweetandbakers-droid <alharamsweetandbakers@gmail.com>
2026-07-25 23:27:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
+// Toggle Menu
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
 
 // Close menu after clicking any link
-
-const links = document.querySelectorAll("#nav-links a");
-
-links.forEach(link => {
+document.querySelectorAll("#nav-links a").forEach(link => {
     link.addEventListener("click", () => {
         navLinks.classList.remove("active");
     });
+});
+
+// Close menu when clicking outside
+document.addEventListener("click", (e) => {
+    if (!menuToggle.contains(e.target) && !navLinks.contains(e.target)) {
+        navLinks.classList.remove("active");
+    }
 });
